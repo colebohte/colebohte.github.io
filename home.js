96d71bbb373e3d67c545567bb3d5cecdcbe25e85
colebohte <colebohte9089@gmail.com>
2025-02-14 17:38:18 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings : ["Developer", "Designer", "Content Creator", "Game Enthusiast"],
+    strings : ["Developer", "Designer", "Content Creator", "Game Enthusiast", "Guy trying to redesign this website"],
     typeSpeed : 75,
     backSpeed : 25,
     loop : true
