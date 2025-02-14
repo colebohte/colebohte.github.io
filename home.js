4220f2d88f1b19c0e889923511e76006525795dc
colebohte <colebohte9089@gmail.com>
2025-02-14 17:43:32 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings : ["Developer", "Designer", "Content Creator", "Game Enthusiast", "Guy trying to redesign this website because it is poorly optimized and designed + I rebranded :)"],
+    strings : ["Developer", "Designer", "Content Creator", "Game Enthusiast", "Guy trying to redesign this website because it is poorly optimized and designed + I rebranded. Another funny thing, if you are using an older browser, you cant even see this :)"],
     typeSpeed : 75,
     backSpeed : 25,
     loop : true
